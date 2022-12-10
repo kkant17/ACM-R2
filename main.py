0779dddb2494048234f0f3122f93f472cd867913
kkant17 <118473890+kkant17@users.noreply.github.com>
2022-12-10 10:29:26 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,80 +1,25 @@
-from qiskit import QuantumCircuit, assemble, Aer
-from qiskit.visualization import plot_histogram
-from qiskit_textbook.widgets import binary_widget
-binary_widget(nbits=5)
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt # plotting
+import numpy as np # linear algebra
+import os # accessing directory structure
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
-#quantum circuit
-qc_output = QuantumCircuit(8)
-qc_output.measure_all()
-qc_output.draw(initial_state=True)
-sim = Aer.get_backend('aer_simulator')
-result = sim.run(qc_output).result()
-counts = result.get_counts()
-plot_histogram(counts)
+for dirname, _, filenames in os.walk('/kaggle/input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
 
-# adder circuit
-qc_encode = QuantumCircuit(8)
-qc_encode.x(7)
-qc_encode.draw()
-qc_encode.measure_all()
-qc_encode.draw()
+nRowsRead = 1000 # specify 'None' if want to read whole file
+# creditcard.csv may have more rows in reality, but we are only loading/previewing the first 1000 rows
+df1 = pd.read_csv('/kaggle/input/creditcard.csv', delimiter=',', nrows = nRowsRead)
+df1.dataframeName = 'creditcard.csv'
+nRow, nCol = df1.shape
+print(f'There are {nRow} rows and {nCol} columns')
 
-sim = Aer.get_backend('aer_simulator')
-result = sim.run(qc_encode).result()
-counts = result.get_counts()
-plot_histogram(counts)
-qc_encode = QuantumCircuit(8)
-qc_encode.x(1)
-qc_encode.x(5)
+df1.head(5)
 
-qc_encode.draw()
+plotPerColumnDistribution(df1, 10, 5)
 
-# controlled-not gate
-qc_cnot = QuantumCircuit(2)
-qc_cnot.cx(0,1)
-qc_cnot.draw()
+plotCorrelationMatrix(df1, 8)
 
-qc = QuantumCircuit(2,2)
-qc.x(0)
-qc.cx(0,1)
-qc.measure(0,0)
-qc.measure(1,1)
-qc.draw()
-
-# half adder
-qc_ha = QuantumCircuit(4,2)
-qc_ha.x(0)
-qc_ha.x(1)
-qc_ha.barrier()
-
-qc_ha.cx(0,2)
-qc_ha.cx(1,2)
-qc_ha.barrier()
-
-qc_ha.measure(2,0)
-qc_ha.measure(3,1)
-
-qc_ha.draw()
-
-# toffoli
-qc_ha = QuantumCircuit(4,2)
-
-qc_ha.x(0)
-qc_ha.x(1)
-qc_ha.barrier()
-
-qc_ha.cx(0,2)
-qc_ha.cx(1,2)
-
-qc_ha.ccx(0,1,3)
-qc_ha.barrier()
-
-qc_ha.measure(2,0)
-qc_ha.measure(3,1)
-
-qc_ha.draw()
-
-# 1+1
-qobj = assemble(qc_ha)
-counts = sim.run(qobj).result().get_counts()
-plot_histogram(counts)
+plotScatterMatrix(df1, 20, 10)
